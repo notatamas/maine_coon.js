3469c0134488db3f6f7db6ee8ef4efe836d650c7
seenxu <seen_xu@msn.com>
2014-03-17 23:52:09 +0100

    fix identation

--- maine_coon.js
+++ maine_coon.js
@@ -33,18 +33,19 @@ THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 var INFO = xml `
-<plugin name="Maine Conn" version="2.6.6"
-  href="https://github.com/vimpr/vimperator-plugins/raw/master/maine_coon.js"
-  summary = "hide the bottom statusbar"
-  xmlns="http://vimperator.org/namespaces/liberator">
-  <author href="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
-  <license>BSD</license>
-  <project name="Vimperator" minVersion="3.6.1"/>
-  <p>
+<plugin name="Maine Conn" version="2.6.7"
+    href="https://github.com/vimpr/vimperator-plugins/raw/master/maine_coon.js"
+    summary = "hide the bottom statusbar"
+    xmlns="http://vimperator.org/namespaces/liberator">
+    <author href="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+    <license>BSD</license>
+    <project name="Vimperator" minVersion="3.7.1"/>
+    <p>
     <code><![CDATA[
-      == Requires ==
+    == Requires ==
         _libly.js
-      == Options ==
+
+    == Options ==
         mainecoon:
           Possible values
             c:
@@ -65,299 +66,309 @@ var INFO = xml `
             :set mainecoon=ac
           ||<
           The default value of this option is "amu".
-          === note ===
+        === note ===
             The C and c options probably are supported on some OSs only.
-      == Global Variables ==
+
+    == Global Variables ==
         maine_coon_targets:
-          Other elements IDs that you want to hide.
-          let g:maine_coon_targets = "sidebar-2 sidebar-2-splitter"
+            Other elements IDs that you want to hide.
+            let g:maine_coon_targets = "sidebar-2 sidebar-2-splitter"
+
         maine_coon_default:
-          The default value of 'mainecoon' option.
-          >||
-            let g:maine_coon_default = "ac"
-          ||<
+            The default value of 'mainecoon' option.
+            >||
+                let g:maine_coon_default = "ac"
+            ||<
+
         maine_coon_style:
-          The Style for message output.
-          >||
+            The Style for message output.
+            >||
             let g:maine_coon_style = "border: 1px solid pink; padding: 3px; color: pink; background: black; font: 18px/1 sans-serif;"
-          ||<
-      == Thanks ==
+            ||<
+
+    == Thanks ==
         * snaka72 (hidechrome part):
           http://vimperator.g.hatena.ne.jp/snaka72/20090106/1231262955
         * seenxu
           make maine_coon.js works with vimperator 3.6.1)
-      == Maine Coon ==
+
+    == Maine Coon ==
         http://en.wikipedia.org/wiki/Maine_Coon
+
     ]]></code>
   </p>
 </plugin>`;
 
 (function () {
 
-  let U = libly.$U;
-  let mainWindow = document.getElementById('main-window');
-  let messageBox = document.getElementById('liberator-message');
-  let bottomBar = document.getElementById('liberator-bottombar');
-  let commandlineBox = document.getElementById('liberator-commandline-command');
+    let U = libly.$U;
+    let mainWindow = document.getElementById('main-window');
+    let messageBox = document.getElementById('liberator-message');
+    let bottomBar = document.getElementById('liberator-bottombar');
+    let commandlineBox = document.getElementById('liberator-commandline-command');
 
-  let tagetIDs = (liberator.globalVariables.maine_coon_targets || '').split(/\s+/);
-  let elemStyle =
-    (
-      liberator.globalVariables.maine_coon_style
-      ||
-      libly.$U.toStyleText({
-        height: '1em',
-        margin: 0,
-        padding: '3px',
-        border: '1px solid #b3b3b3',
-        borderLeft: 0,
-        borderBottom: 0,
-        textAlign: 'left',
-        color: '#000',
-        font: '11px/1 sans-serif',
-        background: '#ebebeb'
+    let tagetIDs = (liberator.globalVariables.maine_coon_targets || '').split(/\s+/);
+    let elemStyle = (
+        liberator.globalVariables.maine_coon_style
+        ||
+        libly.$U.toStyleText({
+            height: '1em',
+            margin: 0,
+            padding: '3px',
+            border: '1px solid #b3b3b3',
+            borderLeft: 0,
+            borderBottom: 0,
+            textAlign: 'left',
+            color: '#000',
+            font: '11px/1 sans-serif',
+            background: '#ebebeb'
       })
     );
 
-  function s2b (s, d) !!((!/^(\d+|false)$/i.test(s)|parseInt(s)|!!d*2)&1<<!s);
+    function s2b (s, d) !!((!/^(\d+|false)$/i.test(s)|parseInt(s)|!!d*2)&1<<!s);
 
-  function hideTargets (hide) {
-    tagetIDs.forEach(
-      function (id)
-        let (elem = document.getElementById(id))
-          (elem && (elem.collapsed = hide))
-    );
-  }
+    function hideTargets (hide) {
+        tagetIDs.forEach(
+            function (id) {
+                let (elem = document.getElementById(id))
+                    (elem && (elem.collapsed = hide))
+            }
+        );
+    }
 
-  function getWindowInfo () {
-      let box = mainWindow.boxObject;
-      let x = screenX < 0 ? 0 : screenX;
-      let y = screenY < 0 ? 0 : screenY;
-      let width =  box.width;
-      let height = box.height;
-      let adjustHeight = box.screenY - y; // maybe caption height?
-      let adjustWidth  = (box.screenX - x) * 2;
-      return {
-        x: x,
-        y: y,
-        width: width,
-        height: height,
-        adjustHeight: adjustHeight,
-        adjustWidth: adjustWidth,
-        state: window.windowState
-      };
-  }
+    function getWindowInfo () {
+        let box = mainWindow.boxObject;
+        let x = screenX < 0 ? 0 : screenX;
+        let y = screenY < 0 ? 0 : screenY;
+        let width =  box.width;
+        let height = box.height;
+        let adjustHeight = box.screenY - y; // maybe caption height?
+        let adjustWidth  = (box.screenX - x) * 2;
 
-  function delay (f, t)
-    setTimeout(f, t || 0);
+        return {
+            x: x,
+            y: y,
+            width: width,
+            height: height,
+            adjustHeight: adjustHeight,
+            adjustWidth: adjustWidth,
+            state: window.windowState
+        };
+    }
 
-  function refreshWindow () {
+    function delay (f, t) {
+        setTimeout(f, t || 0);
+    }
+
+    function refreshWindow () {
+        // FIXME
+        let old = window.outerWidth;
+        window.outerWidth = old + 1;
+        window.outerWidth = old;
+    }
+
     // FIXME
-    let old = window.outerWidth;
-    window.outerWidth = old + 1;
-    window.outerWidth = old;
-  }
+    // figure out the following two lines, what is the correct indentation
+    function getHideChrome ()
+        s2b(mainWindow.getAttribute('hidechrome'), false);
 
-  function getHideChrome ()
-    s2b(mainWindow.getAttribute('hidechrome'), false);
+    function hideChrome (hide, maximize) {
+        hide = !!hide;
+        if (getHideChrome() === hide)
+          return;
+        if (hide)
+          windowInfo = getWindowInfo();
+        mainWindow.setAttribute('hidechrome', hide);
+        delay(function () {
+          window.outerWidth = windowInfo.width;
+          window.outerHeight = windowInfo.height + windowInfo.adjustHeight;
+        });
+        if (maximize && windowInfo.state == window.STATE_MAXIMIZED)
+          delay(function () window.maximize());
+        refreshWindow();
+    }
 
-  function hideChrome (hide, maximize) {
-    hide = !!hide;
-    if (getHideChrome() === hide)
-      return;
-    if (hide)
-      windowInfo = getWindowInfo();
-    mainWindow.setAttribute('hidechrome', hide);
-    delay(function () {
-      window.outerWidth = windowInfo.width;
-      window.outerHeight = windowInfo.height + windowInfo.adjustHeight;
-    });
-    if (maximize && windowInfo.state == window.STATE_MAXIMIZED)
-      delay(function () window.maximize());
-    refreshWindow();
-  }
+    function setFullscreen (full) {
+        full = !!full;
+        if (full === !!window.fullScreen)
+            return;
+        window.fullScreen = full;
+        delay(function () {
+            hideTargets(full);
+            document.getElementById('navigator-toolbox').collapsed = full;
+            if (!full)
+                window.maximize();
+        }, 1000); // FIXME
+    }
 
-  function setFullscreen (full) {
-    full = !!full;
-    if (full === !!window.fullScreen)
-      return;
-    window.fullScreen = full;
-    delay(function () {
-      hideTargets(full);
-      document.getElementById('navigator-toolbox').collapsed = full;
-      if (!full)
-        window.maximize();
-    }, 1000); // FIXME
-  }
+    function nothing (value)
+        (value === undefined);
 
-  function nothing (value)
-    (value === undefined);
+    function important (style)
+        style.replace(/(!important)?\s*;/g, ' !important;');
 
-  function important (style)
-    style.replace(/(!important)?\s*;/g, ' !important;');
+    let setAutoHideCommandLine = (function () {
+        let hiddenNodes = [];
 
-  let setAutoHideCommandLine = (function () {
-    let hiddenNodes = [];
+        return function (hide) {
+            hide = !!hide;
 
-    return function (hide) {
-      hide = !!hide;
+            if (hide === autoHideCommandLine)
+                return;
 
-      if (hide === autoHideCommandLine)
-        return;
+            autoHideCommandLine = hide;
 
-      autoHideCommandLine = hide;
-
-      if (hide) {
-        bottomBar.collapsed = true;
-        let cs = messageBox.parentNode.childNodes;
-        hiddenNodes = [];
-        for (let i = 0, l = cs.length, c; i < l; i++) {
-          c = cs[i];
-          if (c.id == 'liberator-commandline')
-            continue;
-          let style = window.getComputedStyle(c, '');
-          hiddenNodes.push([c, c.collapsed, style.display]);
-          if (c.id != 'liberator-message')
-            c.style.display = 'none';
-          c.collapsed = true;
+            if (hide) {
+                bottomBar.collapsed = true;
+                let cs = messageBox.parentNode.childNodes;
+                hiddenNodes = [];
+                for (let i = 0, l = cs.length, c; i < l; i++) {
+                    c = cs[i];
+                    if (c.id == 'liberator-commandline')
+                        continue;
+                    let style = window.getComputedStyle(c, '');
+                    hiddenNodes.push([c, c.collapsed, style.display]);
+                    if (c.id != 'liberator-message')
+                        c.style.display = 'none';
+                    c.collapsed = true;
+                }
+            } else {
+                bottomBar.collapsed = false;
+                hiddenNodes.forEach(
+                function ([c, v, d]) [c.collapsed, c.style.display] = [v, d]);
+            }
         }
-      } else {
-        bottomBar.collapsed = false;
-        hiddenNodes.forEach(
-          function ([c, v, d]) [c.collapsed, c.style.display] = [v, d]);
-      }
+    })();
+
+    function makeTimeoutDelay (f, n) {
+        if (typeof n === 'undefined')
+            n = 1;
+
+        if (n > 1)
+            f = makeTimeoutDelay(f, n - 1);
+
+        return function () setTimeout(f, 10);
     }
-  })();
 
-  function makeTimeoutDelay (f, n) {
-    if (typeof n === 'undefined')
-      n = 1;
+    function focusToCommandline ()
+        commandlineBox.inputField.focus();
 
-    if (n > 1)
-      f = makeTimeoutDelay(f, n - 1);
+    let useEcho = false;
+    let autoHideCommandLine = false;
+    let displayURL = true;
+    let inputting = false;
+    let windowInfo = {};
 
-    return function () setTimeout(f, 10);
-  }
-
-  function focusToCommandline ()
-    commandlineBox.inputField.focus();
-
-  let useEcho = false;
-  let autoHideCommandLine = false;
-  let displayURL = true;
-  let inputting = false;
-  let windowInfo = {};
-
-  {
-    let a = liberator.globalVariables.maine_coon_auto_hide;
-    let d = liberator.globalVariables.maine_coon_default;
-
-    let def = !nothing(d) ? d :
-              nothing(a)  ? 'amu' :
-              s2b(a)      ? 'amu' :
-              'm';
-
-    autocommands.add(
-      'VimperatorEnter',
-      /.*/,
-      function () delay(function () options.get('mainecoon').set(def), 1000)
-    );
-  }
-
-  U.around(commandline, 'input', function (next, args) {
-    let result = next();
-    inputting = true;
-    bottomBar.collapsed = false;
-    focusToCommandline();
-    return result;
-  }, true);
-
-  U.around(commandline, 'open', function (next, args) {
-    let result = next();
-    bottomBar.collapsed = false;
-    focusToCommandline();
-    return result;
-  }, true);
-
-  U.around(commandline, 'close', function (next, args) {
-    if (autoHideCommandLine && !inputting)
-      bottomBar.collapsed = true;
-    return next();
-  }, true);
-
-  U.around(commandline._callbacks.submit, modes.EX, function (next, args) {
-    let r = next();
-    if (autoHideCommandLine && !inputting && !(
-        modes.extended & modes.OUTPUT_MULTILINE))
-      commandline.close();
-    return r;
-  }, true);
-
-  let (
-    callback = function (next) {
-      if (autoHideCommandLine)
-        bottomBar.collapsed = true;
-      inputting = false;
-      return next();
-    }
-  ) {
-    U.around(commandline._callbacks.submit, modes.PROMPT, callback, true);
-    U.around(commandline._callbacks.cancel, modes.PROMPT, callback, true);
-  }
-
-  options.add(
-    ['mainecoon'],
-    'Make big screen like a Maine Coon',
-    'charlist',
-    '',
     {
-      setter: function (value) {
-        function has (c)
-          (value.indexOf(c) >= 0);
+        let a = liberator.globalVariables.maine_coon_auto_hide;
+        let d = liberator.globalVariables.maine_coon_default;
 
-        if (has('f')) {
-          hideChrome(false);
-          delay(function () setFullscreen(true));
-        } else if (has('c')) {
-          setFullscreen(false);
-          delay(function () hideChrome(true));
-        } else if (has('C')) {
-          setFullscreen(false);
-          delay(function () hideChrome(true, true));
-        } else {
-          hideChrome(false);
-          delay(function () setFullscreen(false));
-        }
+        let def = !nothing(d) ? d :
+                  nothing(a)  ? 'amu' :
+                  s2b(a)      ? 'amu' :
+                  'm';
 
-        setAutoHideCommandLine(has('a'));
-        useEcho = has('m');
-        displayURL = has('u');
-
-        return value;
-      },
-      completer: function (context, args) {
-        context.title = ['Value', 'Description'];
-        context.completions = [
-          ['c', 'Hide caption bar'],
-          ['f', 'Fullscreen'],
-          ['a', 'Hide automatically command-line'],
-          ['C', 'Hide caption bar (maximize)'],
-          ['m', 'Displays the message to command-line'],
-          ['u', 'Displays the message of current page URL when page is loaded.'],
-        ];
-      },
-      validater: function (value) /^[cfa]*$/.test(value)
+        autocommands.add(
+          'VimperatorEnter',
+          /.*/,
+          function () delay(function () options.get('mainecoon').set(def), 1000)
+        );
     }
-  );
 
-  // XXX obsolete
-  commands.addUserCommand(
-    ['fullscreen', 'fs'],
-    'Toggle fullscreen mode',
-    function () setFullscreen(!window.fullScreen),
-    {},
-    true
-  );
+    U.around(commandline, 'input', function (next, args) {
+        let result = next();
+        inputting = true;
+        bottomBar.collapsed = false;
+        focusToCommandline();
+        return result;
+    }, true);
+
+    U.around(commandline, 'open', function (next, args) {
+        let result = next();
+        bottomBar.collapsed = false;
+        focusToCommandline();
+        return result;
+    }, true);
+
+    U.around(commandline, 'close', function (next, args) {
+        if (autoHideCommandLine && !inputting)
+            bottomBar.collapsed = true;
+        return next();
+    }, true);
+
+    U.around(commandline._callbacks.submit, modes.EX, function (next, args) {
+        let r = next();
+        if (autoHideCommandLine && !inputting && !(
+            modes.extended & modes.OUTPUT_MULTILINE))
+            commandline.close();
+        return r;
+    }, true);
+
+    let (
+        callback = function (next) {
+        if (autoHideCommandLine)
+            bottomBar.collapsed = true;
+        inputting = false;
+        return next();
+      }
+    ) {
+      U.around(commandline._callbacks.submit, modes.PROMPT, callback, true);
+      U.around(commandline._callbacks.cancel, modes.PROMPT, callback, true);
+    }
+
+    options.add(
+        ['mainecoon'],
+        'Make big screen like a Maine Coon',
+        'charlist',
+        '',
+        {
+            setter: function (value) {
+                function has (c)
+                    (value.indexOf(c) >= 0);
+
+                if (has('f')) {
+                    hideChrome(false);
+                    delay(function () setFullscreen(true));
+                } else if (has('c')) {
+                    setFullscreen(false);
+                    delay(function () hideChrome(true));
+                } else if (has('C')) {
+                    setFullscreen(false);
+                    delay(function () hideChrome(true, true));
+                } else {
+                    hideChrome(false);
+                    delay(function () setFullscreen(false));
+                }
+
+                setAutoHideCommandLine(has('a'));
+                useEcho = has('m');
+                displayURL = has('u');
+
+                return value;
+            },
+            completer: function (context, args) {
+                context.title = ['Value', 'Description'];
+                context.completions = [
+                    ['c', 'Hide caption bar'],
+                    ['f', 'Fullscreen'],
+                    ['a', 'Hide automatically command-line'],
+                    ['C', 'Hide caption bar (maximize)'],
+                    ['m', 'Displays the message to command-line'],
+                    ['u', 'Displays the message of current page URL when page is loaded.'],
+                ];
+            },
+            validater: function (value) /^[cfa]*$/.test(value)
+        }
+    );
+
+    // XXX obsolete
+    commands.addUserCommand(
+        ['fullscreen', 'fs'],
+        'Toggle fullscreen mode',
+        function () setFullscreen(!window.fullScreen),
+        {},
+        true
+    );
 
 })();
 
